ac1b7af8ec41caf0a0f1b75b2b593973b4c9d18c
imff <fuentes.fatima.eff@gmail.com>
2020-10-07 01:41:36 -0500

    My second commit

--- app/src/main/java/com/example/button/MainActivity.java
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("I made a change");
         setContentView(R.layout.activity_main);
     }
 }
